fbdcefeb25f1af8f7342021fd318318e52d079b5
E-Liyon Design <145768469+E-LyonDev@users.noreply.github.com>
2025-07-24 13:21:15 +0100

    1. Set Up a Next.js Project

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,5 +1,13 @@
-const home: React.FC = () =>{
-  return(
-    
-  )
+import Header from "@/components/layout/Header"
+
+
+export default function Home() {
+  return (
+    <div className="relative h-screen">
+      <Header />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <h1 className="text-white text-3xl">Welcome to airbnb app cloning</h1>
+      </div>
+    </div>
+  );
 }
