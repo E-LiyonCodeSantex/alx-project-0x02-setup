dd56b15e3b75ea86bcc6ff6b510a73fe895f7525
E-Liyon Design <145768469+E-LyonDev@users.noreply.github.com>
2025-07-24 16:36:19 +0100

    7. Implement a PostCard Component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from "react";
+// pages/posts.tsx
+import React from "react";
 import PostCard from "@/components/common/PostCard";
 import { type PostProps } from "@/interfaces";
 import Header from "@/components/layout/Header";
 
-export default function PostsPage() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=6")
-      .then((res) => res.json())
-      .then((data) => {
-        const formattedPosts = data.map((post: any) => ({
-          userId: post.userId,
-          title: post.title,
-          content: post.body,
-        }));
-        setPosts(formattedPosts);
-      });
-  }, []);
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
+  const data = await response.json();
 
+  const posts: PostProps[] = data.map((post: any) => ({
+    userId: post.userId,
+    title: post.title,
+    content: post.body,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const PostsPage: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Header />
@@ -37,4 +44,6 @@ export default function PostsPage() {
       </div>
     </div>
   );
-}
+};
+
+export default PostsPage;
